4243a3d8841c9f5f4574f6463a0ebc919947328b
myua0009 <myua0009@student.monash.edu>
2020-12-13 15:34:19 +1100

    tabbar finish
    change  the page path

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -4,22 +4,22 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "pages/首页/首页",
+      pagePath: "/pages/首页/首页",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "首页"
     }, {
-      pagePath: "pages/活动/活动",
+      pagePath: "/pages/活动/活动",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "活动"
     },{
-      pagePath: "pages/杂志/杂志",
+      pagePath: "/pages/杂志/杂志",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "杂志"
     },{
-      pagePath: "pages/电台/电台",
+      pagePath: "/pages/电台/电台",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "电台"
