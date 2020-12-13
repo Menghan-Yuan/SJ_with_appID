91649bdf7be19a2a10e9391e87c9c154db1c3ba9
myua0009 <myua0009@student.monash.edu>
2020-12-13 14:30:08 +1100

    tabbar
    add page and modify the js

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,41 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "pages/首页/首页",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "首页"
+    }, {
+      pagePath: "pages/活动/活动",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "活动"
+    },{
+      pagePath: "pages/杂志/杂志",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "杂志"
+    },{
+      pagePath: "pages/电台/电台",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "电台"
+    }
+  ]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
